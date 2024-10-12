b82c8eaa9a9d7b856d52c906aaaf183da136d4b1
DevrajNITRKL <devrajsingh86313@gmail.com>
2024-10-12 23:22:34 +0530

    Adding elick events

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,44 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            price: 998,
+            title: 'Mobile Phone',
+            qty: '2',
+            img : ''
+
+        }
+        // this.increaseQuantity = this.increaseQuantity(this);;
+    }
+
+    increaseQuantity = () => {
+        console.log('increase',this.state);
+    }
+    decreaseQuantity (){
+        console.log('decrease');
+    }
+    deleteQuantity (){
+        console.log('delete');
+    }
     render () {
+        const {price,title,qty} = this.state;
+
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.images}/>
                 </div>
                 <div className="right-block">
-                    <div>Phone</div>
-                    <div>Rs. 999</div>
-                    <div>Qty: 1</div>
+                    <div>{title}</div>
+                    <div>{price}</div>
+                    <div>{qty}</div>
                     <div className="cart-item-actions">
                         {/* {Buttons} */}
+                        <img className="action-icons" src="https://img.icons8.com/?size=100&id=1501&format=png&color=000000" alt="increase" onClick={this.increaseQuantity}/>
+                        <img className="action-icons" src="https://img.icons8.com/?size=100&id=14088&format=png&color=000000" alt="decrease" onClick={this.decreaseQuantity}/>
+                        <img className="action-icons" src="https://img.icons8.com/?size=100&id=KPhFC2OwpbWV&format=png&color=000000" alt="delete" onClick={this.deleteQuantity}/>
                     </div>
                 </div>
             </div>
